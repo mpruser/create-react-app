7c6093c5cb76285784c213dc8d546a5eafbe5cca
김재관 <mpruser@tmon.co.kr>
2021-03-10 22:42:00 +0900

    chore: 기본 loader 설치 및 설정

--- webpack/webpack.config.core.js
+++ webpack/webpack.config.core.js
@@ -8,4 +8,21 @@ module.exports = {
     path: path.resolve(__dirname, '../dist'),
     filename: '[name].bundle.js',
   },
+  module: {
+    rules: [
+      {
+        test: /\.(jpe?g|png|gif|svg)$/,
+        loader: 'url-loader',
+        options: {
+          name: '[name].[ext]?[hash]',
+          limit: 10000, // 10kb
+        },
+      },
+      {
+        test: /\.(jsx?|tsx?)$/,
+        exclude: /node_modules/,
+        use: [{ loader: 'babel-loader' }, { loader: 'ts-loader' }],
+      },
+    ],
+  },
 };
