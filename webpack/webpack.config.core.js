e2a71ce1c1eb50eb0a630c37f348b693fe3c3e03
김재관 <mpruser@tmon.co.kr>
2021-03-11 01:39:06 +0900

    chore: TerserPlugin 버전 변경 및 설정
    webpack4에서 terser-webpack-plugin^5을 사용할 경우 버전이슈로 에러가 발생하여 다운그레이드함

--- webpack/webpack.config.core.js
+++ webpack/webpack.config.core.js
@@ -10,7 +10,7 @@ module.exports = {
   },
   output: {
     path: path.resolve(__dirname, '../dist'),
-    filename: '[name].bundle.js',
+    filename: '[name].[contenthash].js',
   },
   plugins: [
     new FriendlyErrorsWebpackPlugin(),
