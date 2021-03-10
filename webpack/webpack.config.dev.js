d461c7527a9fe1dc3aa9a6e24c1faa65f0916ed5
김재관 <mpruser@tmon.co.kr>
2021-03-11 01:09:38 +0900

    chore: webpack dev 환경 세팅

--- webpack/webpack.config.dev.js
+++ webpack/webpack.config.dev.js
@@ -1,5 +1,18 @@
-const path = require('path');
+const { HotModuleReplacementPlugin } = require('webpack');
 
 module.exports = {
   mode: 'development',
+  devtool: 'inline-source-map',
+  plugins: [new HotModuleReplacementPlugin()],
+  devServer: {
+    host: '0.0.0.0',
+    historyApiFallback: true,
+    inline: true,
+    hot: true,
+    port: 3111,
+    quiet: true,
+    open: true,
+    overlay: true,
+    stats: 'errors-only',
+  },
 };
